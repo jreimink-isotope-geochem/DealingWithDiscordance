bbf210f338c1dfe5b544ae384e0d9eb557ff63a9
ejschoonover <erik.schoonover@gmail.com>
2023-06-19 10:19:09 -0400

    Update Shiny_app_discordance.R
    Updated comments and changed the numeric inputs

--- Shiny_app_discordance.R
+++ Shiny_app_discordance.R
@@ -10,14 +10,14 @@ reactlog_enable()
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
-      selectInput(
+      selectInput( #builds the input tab to design the input options
         "data_source",
         label = "Data Source Test",
         choices = c("Copy-Paste", "Upload File"),
         selected = "Upload File"
       ),
       
-      conditionalPanel(
+      conditionalPanel(# for the copy paste input, needs to be changed to react table
         condition = "input.data_source == 'Copy-Paste'",
         textAreaInput(
           "data_input",
@@ -28,7 +28,7 @@ ui <- fluidPage(
         )
       ),
       
-      conditionalPanel(
+      conditionalPanel(#just a file upload
         condition = "input.data_source == 'Upload File'",
         fileInput(
           "data_file",
@@ -37,21 +37,19 @@ ui <- fluidPage(
         )
       ),
       
-      actionButton(
+      actionButton(#creates the button for the obserEvent call to reduce
         inputId = "reduce_data",
         label = "Discordance Reduction"
       ),
       
-      sliderInput(
+      selectInput(# drop down for the node spacing
         inputId = "node_space",
         label = "Node Spacing (ma)",
-        min = 1,
-        max = 50,
-        step = 7,
-        value = 15
+        choices = c(0.5,1,5,10,15,20,25,50,100),
+        selected = 25
       ),
       
-      radioButtons(
+      radioButtons( #input for normalizing the uncertainty
         inputId ="normalize_unc",
         label = 'Uncertainty normalization',
         choiceNames = c("Normalized", "Unnormalized"),
@@ -60,7 +58,7 @@ ui <- fluidPage(
         inline = TRUE
       ),
       
-      radioButtons(
+      radioButtons( # input to weight against concordant analysis
         inputId ="data_type",
         label = 'Type of population',
         choiceNames = c("Detrital (weight against concordance)", "Single (no weighting)"),
@@ -69,44 +67,41 @@ ui <- fluidPage(
         inline = TRUE
       ),
       
-      sliderInput(
+      #NEXT inputs pertain to the entry windows for the axes      
+      numericInput( 
         inputId = "start_cut_upper_min",
         label = "Minimum Upper Intercept Value (Ma)",
+        value = 0,
         min = 0,
-        max = 4500,
-        step = 250,
-        value = 0
+        max = 4500
       ),
       
-      sliderInput(
+      numericInput(
         inputId = "start_cut_upper_max",
         label = "Maximum Upper Intercept Value (Ma)",
         min = 0,
         max = 4500,
-        step = 250,
         value = 2000
       ),
       
-      sliderInput(
+      numericInput(
         inputId = "start_cut_lower_min",
         label = "Minimum Lower Intercept Value (Ma)",
         min = 0,
         max = 4500,
-        step = 250,
         value = 0
       ),
       
-      sliderInput(
+      numericInput(
         inputId = "start_cut_lower_max",
         label = "Maximum Lower Intercept Value (Ma)",
         min = 0,
         max = 4500,
-        step = 250,
         value = 2000
       )
     ),
     
-    mainPanel(
+    mainPanel(# sets up the tabs to show outputs
       textOutput("my_csv_name"),  # Shows file name
       tabsetPanel(
         # Tab 1: Output table
@@ -123,9 +118,9 @@ ui <- fluidPage(
         
         # Tab 5: Output plot - Heatmap 2D Histogram
         tabPanel("Heatmap 2D Histogram", plotOutput("Discordance_plot_heat_map"))
+      )
     )
-  )
-))
+  ))
 
 
 
@@ -158,26 +153,33 @@ server <- function(input, output, session) {
   #will do reduction as soon as data is in the system
   observeEvent(input$data_file, {
     # Reduce the data
-    req(input$data_file)
-    Data.raw <- loadData()
+    req(input$data_file) #requires the data to exist to run
+    Data.raw <- loadData() # raw data use the input data from the function above
     
     # Define variables
-    file_name <- input$data_file$name
-    sample.name <- sub(".csv", "", file_name)
+    file_name <- input$data_file$name # used in labeling and titles
+    sample.name <- sub(".csv", "", file_name) # used in reduction naming
     node.spacing <- as.numeric(input$node_space) # node spacing from drop down
     
-    source('App_reduction.R', local = TRUE)
+    source('App_reduction.R', local = TRUE) # sources the reduction code and ties all the inputs to the variables
     
     # Update the reducedData reactiveValues
+    #the reactive variables only change when something else changes. 
+    # here the variables are the plots from the reworked discordant reduction plotting functions
+    # when the values are stored in this list, they can be called elsewhere
+    # they are called as list elements in the output plots
     reducedData$data <- list(xy_plot,
                              heat_map_plot,
                              lower_int_summed)
   })
   
-  #will update data after sliders change and then button is pressed
-  observeEvent(input$reduce_data, {
-    # Reduce the data
+  #will update data after inputs change and then button is pressed
+  # does the same as above and updates the reactiveVar with new plots
+  observeEvent(input$reduce_data, { # calls the input to see if the button is pressed
+    # requires the data to be input for it to try and run
     req(input$data_file)
+    
+    #cleans the data using the function
     Data.raw <- loadData()
     
     # Define variables
@@ -185,18 +187,21 @@ server <- function(input, output, session) {
     sample.name <- sub(".csv", "", file_name)
     node.spacing <- as.numeric(input$node_space) # node spacing from drop down
     
+    # runs the cleaned discordance code and sets all the inputs
+    # this automates all the things we used to do
     source('App_reduction.R', local = TRUE)
     
     # Update the reducedData reactiveValues
-    reducedData$data <- list(xy_plot,
-                             heat_map_plot,
-                             lower_int_summed)
+    reducedData$data <- list(xy_plot, # upper and lower intercept plot
+                             heat_map_plot, # heat map plot
+                             lower_int_summed) # summed lower intercept plot
   })
   
   
   
   
   # Generate file name
+  # over worked but it gets the file name nonetheless
   output$my_csv_name <- renderText({ # Output file name
     # Test if file is selected
     if (!is.null(input$data_file$datapath)) {
@@ -213,13 +218,13 @@ server <- function(input, output, session) {
     data
   })
   
-  # Process the user input and generate output plot
+  # Process the user input and generate output concordia
   output$output_plot <- renderPlot({
     data <- loadData()
     
-    # Generate the plot or perform other functions
     
-    # Example: Concordia Diagram
+    #Concordia Diagram using isoplotR
+    #UPDATE OPTION: add a change to use type 1 or 2
     data_concordia <- read.data(data[, 2:6], ierr = 2, method = 'U-Pb', format = 1)
     concordia(data_concordia, type = 1)
   })
@@ -228,25 +233,26 @@ server <- function(input, output, session) {
   output$Discordance_plot_upper_lower <- renderPlot({
     req(reducedData$data) # Wait for the data to be available
     
-
-   reducedData$data[[1]]
-
+    
+    reducedData$data[[1]] #calls the plot out of the react variable
+    # the syntax is weird here. double brackets first set calls the list, second calls the item
+    
   })
   
   # Summed lower intercept probability plot
   output$Discordance_plot_lower_summed <- renderPlot({
     req(reducedData$data) # Wait for the data to be available
     
-
-    reducedData$data[[3]]
+    
+    reducedData$data[[3]] #calls the plot out of the react variable
   })
   
   # Heatmap 2d histogram plot
   output$Discordance_plot_heat_map <- renderPlot({
-   req(reducedData$data) # Wait for the data to be available
+    req(reducedData$data) # Wait for the data to be available
     
-    reducedData$data[[2]]
-
+    reducedData$data[[2]] #calls the plot out of the react variable
+    
   })
 }
 
@@ -258,4 +264,3 @@ shinyApp(ui = ui, server = server)
 
 
 
-      
